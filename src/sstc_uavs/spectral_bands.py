a92d6e22e090b9457cad79032cb1692883d7577a
José Beltrán <jose.beltran@nateko.lu.se>
2025-11-17 12:47:47 +0100

    missing old changes

---
+++ src/sstc_uavs/spectral_bands.py
@@ -0,0 +1,23 @@
+# WavelengthFWHM: Wavelength Full Width at Half Maximum
+
+dji_multispectral = {
+    'M3M': {
+        'G': {
+            'BandName': 'Green',
+            'CentralWavelength': 560,
+            
+            'WavelengthFWHM': 16},            
+        'R': {
+            'BandName': 'Red',
+            'CentralWavelength': 650,            
+            'WavelengthFWHM': 16},
+        '': {
+            'BandName': 'Red',
+            'CentralWavelength': 650,            
+            'WavelengthFWHM': 16},
+
+
+        
+        'NIR':'Near-InfraRed', 'D': 'RGB'},
+    'P4M': ['RED', 'GRE', 'BLUE', 'NIR', 'REG']
+    }
